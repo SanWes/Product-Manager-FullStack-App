a7082553f67d1a388fd9a61682ddac60006c37cd
Wesley Sanchez <77991913+SanWes@users.noreply.github.com>
2023-12-14 19:33:56 -0500

    Restructure of server configuration

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ app.use( express.json() ); //tells my app that it can parse json
 app.use( express.urlencoded({ extended: true }) ); //tells my app that it can gather form information
 app.use(cors()); //tells the app that it is allowed to share resources  
 
-require("./server/config/config");
+require("./config/config");
 console.log("")
 
 //require the routes
-require("./server/routes/product.routes")(app)
+require("./routes/product.routes")(app)
 
 //app.listen needs to be at the end of the file
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
